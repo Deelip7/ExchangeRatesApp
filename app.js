5640d8a336b63ed093eb237e1667cd7804ad5aea
Theelipan Prabakar <theelipan@gmail.com>
2020-08-13 21:55:08 -0400

    add select option to pick two currencies

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+let currenyOne = document.querySelector("#country1");
+let currenyTwo = document.querySelector("#country2");
+
+currenyOne.addEventListener("change", (e) => {
+  console.log(currenyOne.options[currenyOne.selectedIndex].text);
+});
+
+currenyTwo.addEventListener("change", (e) => {
+  console.log(currenyTwo.options[currenyTwo.selectedIndex].text);
+});
