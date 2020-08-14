b819a3bbfa6a127b522dd3c9e3005d3679d418c8
Theelipan Prabakar <theelipan@gmail.com>
2020-08-13 22:57:12 -0400

    fetch data from api & auto populate option element

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+// import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
-let currenyOne = document.querySelector("#country1");
-let currenyTwo = document.querySelector("#country2");
+/*
+/------------------------------------------------------------
+/   Events
+/------------------------------------------------------------
+/   Get two Select option 
+/
+*/
+const currenyOne = document.querySelector("#country1");
+const currenyTwo = document.querySelector("#country2");
+// document.addEventListener("change", (e) => {
+//   if (e.target.id === "country1") {
+//     console.log(currenyOne.options[currenyOne.selectedIndex].text);
+//   } else {
+//     console.log(currenyTwo.options[currenyTwo.selectedIndex].text);
+//   }
+// });
 
-currenyOne.addEventListener("change", (e) => {
-  console.log(currenyOne.options[currenyOne.selectedIndex].text);
-});
-
-currenyTwo.addEventListener("change", (e) => {
-  console.log(currenyTwo.options[currenyTwo.selectedIndex].text);
-});
+(function loadSelections() {
+  fetch("https://api.exchangeratesapi.io/latest?base=USD")
+    .then((res) => res.json())
+    .then((data) => {
+      const dataKeys = Object.keys(data.rates);
+      dataKeys.forEach((element) => {
+        currenyOne.add(new Option(element));
+        currenyTwo.add(new Option(element));
+      });
+    });
+})();
